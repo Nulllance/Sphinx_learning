0bd7e1a753e885aeed012aeba91ee09abe80da88
lance from huaweiLaptop <gggreenlance@gmail.com>
2023-03-07 12:11:41 +0800

    init commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,23 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'sphinx_learning'
-copyright = '2023, Lance'
-author = 'Lance'
+project = 'Learning Notes'
+copyright = '2023, lance'
+author = 'lance'
 release = '0.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.duration']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,21 @@
 .. sphinx_learning documentation master file, created by
-   sphinx-quickstart on Tue Mar  7 09:21:51 2023.
+   sphinx-quickstart on Tue Mar  7 09:26:59 2023.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to sphinx_learning's documentation!
+Lance Wu's Learning notes
 ===========================================
 
+Contents
+--------
 .. toctree::
+   html
+   css
+   javascript
    :maxdepth: 2
    :caption: Contents:
 
 
 
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
+Check out the :doc:`usage` section for further information, including how to
+:ref:`install <installation>` the project.
